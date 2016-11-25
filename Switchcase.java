d1259b86f6626c53f8a882f6eb154962321a3e21
sprohini1987 <spshalu.1987@gmail.com>
2016-11-26 01:00:00 +0530

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,26 @@
+package com.cts.cond;
+
+import java.util.Scanner;
+
+
+public class Switchcase {
+	public static void main(String[] args) {
+		Switchcase d=new Switchcase();
+	Scanner s=new Scanner(System.in);
+	int a=4,b=5;
+	float c;
+	String g=s.next();{
+	switch(g){
+	case "e":
+		c=a+b;
+		System.out.println(c);
+		break;
+	case "f":
+		c=a-b;
+		System.out.println(c);
+		break;
+	}
+	
+	}
+}
+}
