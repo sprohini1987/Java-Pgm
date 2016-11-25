8999a9a1c312eca327bb1a42eedca9c44245a04d
sprohini1987 <spshalu.1987@gmail.com>
2016-11-26 01:03:11 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,45 @@
+package com.cts.cond;
+
+import java.util.Scanner;
+
+public class whileloop {
+
+	public static void main(String[] args) {
+		whileloop d=new whileloop();
+		int b;
+		float c;
+		int f = 0;
+		while(f==1){
+			int a;
+			Scanner s=new Scanner(System.in);
+			System.out.println("Enter Ur Choice");
+			System.out.println("Press 1 for Add");
+			System.out.println("Press 2 for sub");
+			System.out.println("Press 3 for mul");
+			System.out.println("Press 4 for div");
+			int ch=s.nextInt();
+		if(ch==1){
+			c=a+b;
+			System.out.println("add="+c);
+		}
+		else if(ch==2){
+			c=a-b;
+			System.out.println("sub="+c);
+		}
+		else if(ch==3){
+			c=a*b;
+			System.out.println("Mul="+c);
+		}
+		else if(ch==4){
+			c=a/b;
+			System.out.println("Div="+c);
+		}
+		c=a;
+	
+			System.out.println("do u want to continue");
+			System.out.println("1.yes||2.No");
+			f=s.nextInt();
+	}
+	}
+
+}
