de612b29bc1429c01df92065c9d66063b817ce25
sprohini1987 <spshalu.1987@gmail.com>
2016-11-29 23:59:41 +0530

    Add files via upload

---
+++ ForLoopPgm.java
@@ -0,0 +1,12 @@
+package com.cts.cond;
+
+public class ForLoopPgm {
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++){
+			for(int j=0;j<=i;j++){
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
