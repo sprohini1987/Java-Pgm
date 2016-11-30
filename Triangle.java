6470ecc75a31d29f0cc503c2ad5e0799a629800d
sprohini1987 <spshalu.1987@gmail.com>
2016-12-01 00:21:54 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,16 @@
+package com.cts.cond;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++){
+			for(int j=3;j>i;j--){
+				System.out.print(" ");
+			}
+			for(int k=0;k<=i;k++){
+			System.out.print("* ");
+			}
+System.out.println();	
+	}
+	}
+}
---
+++ Twotriangle.java
@@ -0,0 +1,17 @@
+package com.cts.cond;
+
+public class Twotriangle {
+	public static void main(String[] args) {
+		for(int i=0;i<6;i++){
+			for(int j=3;j>i;j--){
+				System.out.print("*");
+			}
+			for(int k=3;k<=i;k++){
+			System.out.print("*");
+			}
+System.out.println();	
+	}
+
+	}
+
+}
