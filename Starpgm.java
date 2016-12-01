c0a5189f60a50aa1d1c0f8309ab6d438dcc04a73
sprohini1987 <spshalu.1987@gmail.com>
2016-12-01 22:09:01 +0530

    Add files via upload

---
+++ Starpgm.java
@@ -0,0 +1,21 @@
+package com.cts.cond;
+
+public class Starpgm {
+	public static void main(String[] args) {
+		for(int i=0;i<=0;i++){
+			for(int j=1;j>i;j--)
+			{
+				System.out.print("  *\n");
+			}
+			for(int k=1;k>i;k--){
+				System.out.print(" * *\n");
+				System.out.print("*   *\n");
+				System.out.print(" * *\n");
+				System.out.print("  *\n");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
