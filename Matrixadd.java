5c5507f4b27fa3083966729fae9e5f8fd539eae7
sprohini1987 <spshalu.1987@gmail.com>
2016-12-02 13:58:24 +0530

    Add files via upload

---
+++ Matrixadd.java
@@ -0,0 +1,38 @@
+package com.tcs.array;
+
+import java.util.Scanner;
+
+public class Matrixadd {
+
+	public static void main(String[] args) {
+		int a[][]={{6,9},{4,7}};
+		int b[][]={{7,6},{5,9}};
+		int i,j;
+		int c[][];
+		for(i=0;i<=1;i++){
+			for(j=0;j<=1;j++){
+				
+			System.out.print(a[i][j]+" ");
+		}
+			System.out.println();
+		}
+			for(i=0;i<=1;i++){
+				for(j=0;j<=1;j++){
+					
+					System.out.print(b[i][j]+" ");
+				}
+			System.out.println();
+	}
+			for(i=0;i<=1;i++){
+			for(j=0;j<=1;j++){
+				c[i][j]=a[i][j]+b[i][j];
+		}
+			for(i=0;i<=1;i++){
+				for(j=0;j<=1;j++){
+				System.out.print(c[i][j]+" ");
+			}
+			System.out.println();
+	}
+
+}
+}}
