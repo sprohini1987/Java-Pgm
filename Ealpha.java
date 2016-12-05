49f8e303314f4a8102f7cad96e6563eb03387ea2
sprohini1987 <spshalu.1987@gmail.com>
2016-12-06 00:48:27 +0530

    Add files via upload

---
+++ Ealpha.java
@@ -0,0 +1,18 @@
+package com.cts.cond;
+
+public class Ealpha {
+
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+				System.out.print("* ");
+}
+			for(int k=2;k>=i;k--)
+					{
+				System.out.println("* ");
+	
+}
+}
+			System.out.println();
+		}
+	}
---
+++ OppTri.java
@@ -0,0 +1,19 @@
+package com.cts.cond;
+
+public class OppTri {
+
+	public static void main(String[] args) {
+		for(int i=0;i<3;i++){
+			for(int j=0;j<i;j++){
+				System.out.print(" ");
+				}
+			for(int k=3;k>i;k--)
+			{
+				System.out.print("*");
+			}
+		System.out.println();
+}
+		
+	}
+
+}
