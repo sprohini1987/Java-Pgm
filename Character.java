025eb3381127c39288115c68bd1e75564c008537
sprohini1987 <spshalu.1987@gmail.com>
2016-12-11 00:20:49 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,22 @@
+package com.tcs.array;
+
+class Character {
+
+	public static void main(String[] args) {
+		
+    char g[]={'u','l','h','O','p','e','A','d','I'};
+		for(int i=0;i<g.length;i++){
+			if(g[i]=='a'||g[i]=='e'||g[i]=='i'||g[i]=='o'||g[i]=='u')
+			{
+				System.out.println(g[i]);
+			}
+			if(g[i]=='A'||g[i]=='E'||g[i]=='I'||g[i]=='O'||g[i]=='U')
+			{
+			System.out.println(g[i]);
+		
+
+	}
+
+}
+	}
+}
