00d66f7d0823618af29d2a7eecaf5f2b37803856
sprohini1987 <spshalu.1987@gmail.com>
2016-12-19 00:25:26 +0530

    Add files via upload

---
+++ Foreg2.java
@@ -0,0 +1,13 @@
+package com.tcs.array;
+
+public class Foreg2 {
+
+	public static void main(String[] args) {
+		int i=5;
+		System.out.println(i++);
+		System.out.println(i--);
+		System.out.println(++i);
+		System.out.println(--i);
+	}
+
+}
