d6650856b4dd57afc0f92e7fcaec87453f365327
sprohini1987 <spshalu.1987@gmail.com>
2016-12-19 00:29:13 +0530

    Add files via upload

---
+++ Foreg3.java
@@ -0,0 +1,13 @@
+package com.tcs.array;
+
+public class Foreg3 {
+
+	public static void main(String[] args) {
+	int i;
+		for(i=0;i<5;i++){
+			System.out.println(i);
+		}
+		System.out.println("value of i="+i);
+	}
+
+}
