fb8a6a1e7f463b840e8e1b1b199b81b6068ce432
sprohini1987 <spshalu.1987@gmail.com>
2016-12-21 13:21:31 +0530

    Add files via upload

---
+++ Polindrome.java
@@ -0,0 +1,26 @@
+package Loopingprg;
+
+public class Polindrome {
+
+	public static void main(String[] args) {
+		int n[]={23,121,656,899,1001};
+		for(int i=0;i<n.length;i++){
+			int n1=n[i];
+			int reverse=0;
+			int temp=0;
+			while(n1>0){
+				temp=n1%10;
+				n1=n1/10;
+				reverse=reverse*10+temp;
+			}
+			if(n[i]==reverse){
+				System.out.println(n[i]+ " polindrome num");
+			}
+				else{
+					System.out.println(n[i]+" not polindrome num");
+				}
+			}
+			}
+		}
+
+
