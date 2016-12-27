32ebb058f7c49dca85c860ad0358421fdc27f6b9
sprohini1987 <spshalu.1987@gmail.com>
2016-12-27 23:31:42 +0530

    Add files via upload

--- Switchcase.java
+++ Switchcase.java
@@ -1,26 +1,74 @@
 package com.cts.cond;
 
 import java.util.Scanner;
-
-
 public class Switchcase {
 	public static void main(String[] args) {
-		Switchcase d=new Switchcase();
+    Switchcase d=new Switchcase();
 	Scanner s=new Scanner(System.in);
-	int a=4,b=5;
+	System.out.println("a=");
+	int a=s.nextInt();
+	System.out.println("b=");
+	int b=s.nextInt();
 	float c;
-	String g=s.next();{
+	System.out.println("press 1 for add;  2 for sub;   3 for multiplication;  4 for division");
+	int g=s.nextInt();
+	for(int i=1;i<=g;i++){
 	switch(g){
-	case "e":
+	case 1:
 		c=a+b;
-		System.out.println(c);
+		System.out.println("add="+c);
+		break;
+	case 2:
+		c=a-b;
+		System.out.println("sub="+c);
+		break;
+	case 3:
+		c=a+b;
+		System.out.println("mul="+c);
+		break;
+	case 4:
+		c=a/b;
+		System.out.println("div="+c);
+	}
+	}
+	System.out.println("do u want to continue the operation");
+	System.out.println("press 1 for yes ");
+	int yes=s.nextInt();
+	if(yes==1){
+		System.out.println("q=");
+		int q=s.nextInt();
+		System.out.println("t=");
+		int t=s.nextInt();
+		float cc;
+		System.out.println("press e for add;  f for sub;   h for multiplication;  j for division");
+	String k=s.next();
+	switch(k){
+	case "e":
+		cc=q+t;
+		System.out.println("add="+cc);
 		break;
 	case "f":
-		c=a-b;
-		System.out.println(c);
+		cc=q-t;
+		System.out.println("sub="+cc);
 		break;
+	case "h":
+		cc=q*t;
+		System.out.println("mul="+cc);
+		break;
+	case "j":
+		cc=q/t;
+		System.out.println("div="+cc);
+	}
+	}
+	else {
+		System.out.println("stop");
+	}
+	}
 	}
 	
-	}
-}
-}
+		
+	
+
+	
+	
+
