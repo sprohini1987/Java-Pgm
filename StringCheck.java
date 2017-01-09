587d4439494a200097fd5ee3a70f5fc9511fc087
sprohini1987 <spshalu.1987@gmail.com>
2017-01-10 00:02:42 +0530

    Add files via upload

--- StringCheck.java
+++ StringCheck.java
@@ -6,10 +6,15 @@ package com.ccc.inter;
 		public static void main(String[] args) {
 			String a = "Good"; 
 			String b = new String("Good"); 
-			if(a=="Good") 
+			if(a==b) 
 				System.out.println("a & b are equal");
 			else System.out.println("Not Equal");
-			
+			if(a.equals(b)){
+				System.out.println("a is equal to b");
+			}
+			else{
+				System.out.println("a not equal to b");
+			}
 
 		}
 
